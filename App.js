57c5e33ac6373948c4996f6620bb3a0f7c530fde
youssefatti <youssefattia@gmail.com>
2018-03-31 11:40:56 +0200

    add-ios-camera

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StackNavigator } from "react-navigation";
 import LoginScreen from "./src/screens/Login/LoginScreen";
 import Home from "./src/screens/Home/Home";
+import Picture from "./src/screens/Picture/Picture";
 
 const App = StackNavigator({
   LogIn: {
@@ -8,6 +9,9 @@ const App = StackNavigator({
   },
   Home: {
     screen: Home
+  },
+  Picture: {
+    screen: Picture
   }
 });
 console.ignoredYellowBox = ["Warning: componentWill"];
